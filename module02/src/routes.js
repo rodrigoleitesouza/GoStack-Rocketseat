5781a38f621461433ff65c0a0a41bc5e9f1fd204
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-08 16:17:21 -0300

    Rocketing one more time!

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-    return res.json({ message: "Hello, Rodrigo333!"});    
+    return res.json({ message: "Hello, Rodrigo333!"});
 })
 
 export default routes;
