9167775b13cd2598e9a2164a65dffb0331a3cf0d
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-09 15:51:05 -0300

    Keep going!

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 
+import User from "./app/models/User";
+
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-    return res.json({ message: "Hello, Rodrigo333!"});    
-})
+routes.get("/", async (req, res) => {
+    const user = await User.create({
+      name: "Luciane Souza",
+      email: "lucidrigo@gmail.com",
+      password_hash: "9999999",
+    });
+
+    return res.json(user);
+});
 
 export default routes;
