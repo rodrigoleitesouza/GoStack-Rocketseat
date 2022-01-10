732f46b95e2b963d853fefbd1cb86b35c0838fec
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-10 19:54:15 -0300

    A good day!

--- module02/src/app/controllers/SessionController.js
+++ module02/src/app/controllers/SessionController.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import * as Yup from "yup";
 
 import User from "../models/User";
 
@@ -6,32 +7,43 @@ import authConfig from "../../config/auth";
 import auth from "../../config/auth";
 
 class SessionController {
-    async store(req, res) {
-        const { email, password } = req.body;
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
 
-        const user = await User.findOne({ where: { email } });
-
-        if(!user) {
-            return res.status(401).json({ error: "User not found." });
-        }
-
-        if(!(await user.checkPassword(password))) {
-            return res.status(401).json({ error: "Password does not match." })
-        }
-
-        const { id, name } = user;
-
-        return res.json({
-            user: {
-                id,
-                name,
-                email,
-            },
-            token: jwt.sign({ id }, authConfig.secret, {
-                expiresIn: authConfig.expiresIn,
-            }),
-        })
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation fails." });
     }
+
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: "User not found." });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: "Password does not match." })
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    })
+  }
 }
 
 
