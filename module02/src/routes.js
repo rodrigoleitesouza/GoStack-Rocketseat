cfb802d6da0bb7cdff2419418599f09c1b5c25ca
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-10 17:14:52 -0300

    We are going!

--- module02/src/routes.js
+++ module02/src/routes.js
@@ -1,17 +1,11 @@
 import { Router } from "express";
 
-import User from "./app/models/User";
+import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
 
 const routes = new Router();
 
-routes.get("/", async (req, res) => {
-    const user = await User.create({
-      name: "Luciane Souza",
-      email: "lucidrigo@gmail.com",
-      password_hash: "9999999",
-    });
-
-    return res.json(user);
-});
+routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
 
 export default routes;
