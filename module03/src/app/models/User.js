c7d20025171de1d06fd09a519b4ab14077a9c0e6
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-12 16:38:57 -0300

    You can't stop me!

--- module03/src/app/models/User.js
+++ module03/src/app/models/User.js
@@ -19,8 +19,12 @@ class User extends Model {
       if(user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
-    }); 
-    return this; 
+    });
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: "avatar_id" });
   }
 
   checkPassword(password) {
