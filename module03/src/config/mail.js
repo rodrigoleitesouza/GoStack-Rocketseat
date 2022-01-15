8940bb0b5094d8c02a828746d5ae0914bdf2e127
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-15 19:23:12 -0300

    What a lovely day!

---
+++ module03/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "585f42bf06af42",
+    pass: "3da01727375c8c",
+  },
+  default: {
+    from: "Equipe GoBarber <noreply@gobarber.com>",
+  },
+};
