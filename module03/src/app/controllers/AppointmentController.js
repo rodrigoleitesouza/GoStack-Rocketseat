15bf64964a6367b80eca573073bce30beb5d8d6d
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-14 21:04:50 -0300

    Hurricane!

--- module03/src/app/controllers/AppointmentController.js
+++ module03/src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from "yup";
-import { startOfHour, parseISO, isBefore } from "date-fns";
+import { startOfHour, parseISO, isBefore, format } from "date-fns";
+import pt from "date-fns/locale/pt";
 import User from "../models/User";
 import File from "../models/File";
 import Appointment from "../models/Appointment";
+import Notification from "../schemas/Notification";
 
 
 class AppointmentController {
@@ -53,7 +55,7 @@ class AppointmentController {
 
     if (!isProvider) {
       return res
-      .status(400)
+      .status(401)
       .json({ error: "You can only create appointments with providers."});
     }
 
@@ -79,6 +81,18 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às 'H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}.`,
+      user: provider_id,
     });
 
     return res.json(appointment);
