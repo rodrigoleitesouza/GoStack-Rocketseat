adc4136f3c65ae36a7fcd42a536bfe3e50be7017
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-19 14:36:06 -0300

    On my way to top!

---
+++ module04/src/components/TechItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TechItem({ tech, onDelete }) {
+  return (
+    <li>
+        {tech}
+        <button onClick={onDelete} type="button">Remover</button>
+    </li>
+  );
+}
+
+
+export default TechItem;
--- module04/src/components/TechList.js
+++ module04/src/components/TechList.js
@@ -1,23 +1,47 @@
 import React, { Component } from "react";
 
+import TechItem from "./TechItem";
+
 class TechList extends Component {
   state = {
+    newTech: "",
     techs: [
       "Node.js",
       "ReactJS",
-      "React Native222",
+      "React Native22",
     ]
   };
 
-  render() {
-    console.log(this.state);
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
 
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: "", 
+    });
+  }
+
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+  }
+
+  render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => < TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)} />)}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     ); 
   }
 }
