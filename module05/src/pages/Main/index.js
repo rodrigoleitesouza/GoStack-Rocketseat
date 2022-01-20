7d417ebb8c35e93a75a3a901292a6e6ba55bbc33
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-20 15:22:27 -0300

    Anyone can't stop me!

--- module05/src/pages/Main/index.js
+++ module05/src/pages/Main/index.js
@@ -1,7 +1,6 @@
 import React from "react";
+import { Title } from "./styles";
 
-function Main() {
-  return <h1>Main</h1>;
+export default function Main() {
+  return <Title>Main</Title>;
 }
-
-export default Main;
