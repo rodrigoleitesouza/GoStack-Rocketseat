3bc93ecef3d7b4293ae93b81e45bba1d3eaa438d
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-21 17:53:53 -0300

    I can do it!

--- module05/src/pages/Repository/index.js
+++ module05/src/pages/Repository/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-function Repository() {
-  return <h1>Repository</h1>;
+export default function Repository({ match }) {
+  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
 }
-
-export default Repository;
