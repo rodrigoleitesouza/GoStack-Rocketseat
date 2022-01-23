f0cb6ba61f078a3ca97d9f81145c0f157a842360
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-23 13:27:05 -0300

    Setting Things Up!

--- module06/App.js
+++ module06/App.js
@@ -1,16 +1,5 @@
-import React, {Component} from "react";
-import {Platform, StyleSheet, Text, View} from "react-native";
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Apenas um teste!</Text>
-        <Text style={styles.welcome}>Abaixo!</Text>
-      </View>
-    );
-  }
-}
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -25,3 +14,13 @@ const styles = StyleSheet.create({
     margin: 10,
   }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Apenas um teste!</Text>
+      <Text style={styles.welcome}>Abaixo!</Text>
+    </View>
+  );
+}
+
