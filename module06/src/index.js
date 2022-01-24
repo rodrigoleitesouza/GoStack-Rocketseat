1ac6837487b33867ffce44c6c9b5dc511039dfd8
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-24 20:50:27 -0300

    Ok, lets teste it!

--- module06/src/index.js
+++ module06/src/index.js
@@ -1,15 +1,29 @@
 import React from "react";
-import { StatusBar } from "react";
-
+import { StyleSheet, Text, View } from "react-native";
 import "./config/ReactotronConfig";
 
-import Routes from "./routes";
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  }
+});
+
+console.tron.log("Hello, World22!");
 
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159C1" />
-      <Routes />
-    </>
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Apenas um teste!</Text>
+      <Text style={styles.welcome}>Abaix4444o!</Text>
+    </View>
   );
 }
+
