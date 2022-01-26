a9fe3b519c590c141c408e2797f755b9f1cdff1b
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-26 19:31:08 -0300

    Here its working!

--- module06/src/pages/Main/index.js
+++ module06/src/pages/Main/index.js
@@ -1,27 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Keyboard, ActivityIndicator } from "react-native"
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { View, Text } from 'react-native';
-import { Container, Form, Input, /*SubmitButton*/ } from './styles';
+import api from '../../services/api';
 
-const Main = () => {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
+
+export default class Main extends Component {
+
+  state = {
+    newUser: '',
+    users: [],
+    loading: false,
+  };
+
+
+
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    this.setState({ loading: true });
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: "",
+      loading: false,
+    });
+
+    Keyboard.dismiss();
+  };
+
+
+  render() {
+    const { users, newUser, loading } = this.state;
+    //console.tron.log("KKKKK");
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            { loading ? <ActivityIndicator color="#FFF" /> : <Icon name="add" size={20} color="#FFF" /> }
+          </SubmitButton>
+        </Form>
+
+        <List
+          data={users}
+          keyExtractor={user => user.login}
+          renderItem={({ item }) => (
+            <User>
+              <Avatar source={{ uri: item.avatar }} />
+              <Name>{item.name}</Name>
+              <Bio>{item.bio}</Bio>
+
+              <ProfileButton onPress={() => { }}>
+                <ProfileButtonText>Ver perfil</ProfileButtonText>
+              </ProfileButton>
+            </User>
+          )}
         />
-
-      </Form>
-    </Container>
-  );
+      </Container>
+    );
+  }
 };
-
-export default Main;
-
-/*
-<SubmitButton>
-  <Icon name="add" size={20} color="#FFF" />
-</SubmitButton>
-        */
