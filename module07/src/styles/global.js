d00b2ea208451f12168fa8efa04de7cd6ff00542
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-01-31 10:53:23 -0300

    Stylish!

---
+++ module07/src/styles/global.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+//import background from "../assets/images/background.svg"
+
+export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background: #191920;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, button {
+        font: 14px Roboto, sans-serif;
+    }
+
+    #root {
+        max-width: 1020px;
+        margin: 0 auto;
+        padding: 0 20px 50px;
+    }
+
+    button {
+        cursos: pointer;
+    }
+`;
