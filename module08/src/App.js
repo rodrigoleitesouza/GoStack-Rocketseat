fe63519c8e058a9806b3c2fcf8d5e6f9cd874851
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-02-03 20:27:25 -0300

    Hooking!

--- module08/src/App.js
+++ module08/src/App.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS','React Native']);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
-  function handleAdd() {
+  const handleAdd = useCallback(() => {
     setTech([...tech, newTech]);
     setNewTech('');
-  }
+  }, [newTech, tech]);
 
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech))
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
 
   return (
     <>
@@ -17,6 +30,9 @@ function App() {
       </ul>
       <input value={newTech} onChange={e => setNewTech(e.target.value)} />
       <button type='button' onClick={handleAdd}>Adicionar</button>
+      <br/>
+      <br/>
+      <strong>Você tem {techSize} tecnologia(s).</strong>
     </>
   );
 }
