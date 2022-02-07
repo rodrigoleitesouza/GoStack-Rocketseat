1cb5f97829737172879f61ba1cdd20902e70502a
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-02-07 19:13:30 -0300

    Working well!

--- module09/src/routes/Route.js
+++ module09/src/routes/Route.js
@@ -2,15 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Route, Redirect } from "react-router-dom";
 
-import AuthLayout from "../pages/_layouts/auth";
-import DefaultLayout from "../pages/_layouts/default";
+import AuthLayout from "~/pages/_layouts/auth";
+import DefaultLayout from "~/pages/_layouts/default";
 
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = true;
+  const signed = false;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
---
+++ module09/src/styles/global.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
