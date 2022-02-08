478ef4e7857389b4e6ced599bfedad01ed367c9d
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-02-08 19:47:17 -0300

    The wind!

--- module09/src/pages/SignUp/index.js
+++ module09/src/pages/SignUp/index.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "~/assets/logo.svg";
 
 export default function SignUp() {
-  return <h1>Register Page!</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="text" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
