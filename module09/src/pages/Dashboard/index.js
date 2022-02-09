a2fd00138d494ff20df6c1c5899b98c72a718296
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-02-09 20:16:02 -0300

    Anyway is possible!

--- module09/src/pages/Dashboard/index.js
+++ module09/src/pages/Dashboard/index.js
@@ -1,5 +1,8 @@
 import React from "react";
+import api from "../../services/api";
 
 export default function Dashboard() {
+  api.get("appointments");
+
   return <h1>Dashboard Page!</h1>;
 }
--- module09/src/store/modules/auth/sagas.js
+++ module09/src/store/modules/auth/sagas.js
@@ -22,6 +22,8 @@ export function* signIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push("/dashboard");
@@ -31,6 +33,37 @@ export function* signIn({ payload }) {
   }
 }
 
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, "users", {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push("/");
+  } catch (err) {
+    toast.error("Falha no cadastro, verifique seus dados!");
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest("persist/REHYDRATE", setToken),
   takeLatest("@auth/SIGN_IN_REQUEST", signIn),
+  takeLatest("@auth/SIGN_UP_REQUEST", signUp),
 ]);
