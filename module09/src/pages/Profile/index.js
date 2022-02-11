31158d148767fde14243d1b18406a201327872ab
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-02-11 13:51:08 -0300

    Notificating!

--- module09/src/pages/Profile/index.js
+++ module09/src/pages/Profile/index.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Form, Input } from "@rocketseat/unform";
+
+import { Container } from "./styles";
 
 export default function Profile() {
-  return <h1>Profile Page!</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+
+  }
+
+  return <Container>
+    <Form initialData={profile} onSubmit={handleSubmit}>
+      <Input name="name" placeholder="Nome completo" />
+      <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+      <hr />
+
+      <Input name="oldPassword" type="password" placeholder="Sua senha atual" />
+      <Input name="password" type="password" placeholder="Nova senha" />
+      <Input name="confirmPassword" type="password" placeholder="Confirmação de nova senha" />
+
+      <button type="submit">Atualizar perfil</button>
+    </Form>
+
+    <button type="button">Sair do GoBarber!</button>
+  </Container>;
 }
