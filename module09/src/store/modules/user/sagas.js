16d89b21b4161749a7b78d7336914436338cad1d
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-02-11 14:54:50 -0300

    Keep rocketing!

--- module09/src/store/modules/user/sagas.js
+++ module09/src/store/modules/user/sagas.js
@@ -1,5 +1,24 @@
-import { all } from "redux-saga/effects";
+import { takeLatest, call, put, all } from "redux-saga/effects";
+import { toast } from "react-toastify";
+import api from "~/services/api";
+
+import {} from "";
+
+export function* updateProfile({ payload }) {
+  const { name, email, ...rest } = payload.data;
+
+  const profile = Object.assign(
+    { name, email },
+    rest.oldPassword ? rest : {}
+  );
+
+  const response = yield call(api.put, "users", profile);
+
+  toast.success("Perfil atualizado com sucesso!");
+
+  yield put(updateProfileSuccess)
+}
 
 export default all([
-
+  takeLatest("@user/UPDATE_PROFILE_REQUEST", updateProfile),
 ]);
