0c0aa6ac4e83ec8647e130ecdc7929a41ad93e65
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-02-16 19:19:28 -0300

    Working!

--- module10/src/pages/SignIn/index.js
+++ module10/src/pages/SignIn/index.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-import Input from '../../components/Input';
-import Button from '../../components/Button';
+import logo from '../../assets/logo.png';
+
 import Background from '../../components/Background';
 
-const SignIn = () => {
+import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles.js';
+
+export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn Page</Text>
+      <Container>
+        <Image source={logo} />
 
-      <Input icon='call' placeholder='Digite seu nome'/>
-      <Button>Entrar</Button>
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>
+            Acessar
+          </SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   )
 }
 
-export default SignIn;
