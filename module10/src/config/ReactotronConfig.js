56991acab844c6c07b339fc25ba79e09ab0c7639
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-02-18 18:06:26 -0300

    Testing!

--- module10/src/config/ReactotronConfig.js
+++ module10/src/config/ReactotronConfig.js
@@ -1,14 +1,15 @@
-import Reactotron, { asyncStorage } from 'reactotron-react-native'
+import Reactotron, { asyncStorage } from 'reactotron-react-native';
 import { reactotronRedux } from "reactotron-redux";
 import reactotronSaga from "reactotron-redux-saga";
-import { AsyncStorage } from '@react-native-community/async-storage';
+//import AsyncStorage from '@react-native-community/async-storage';
+import { AsyncStorage } from "react-native";
 
 if (__DEV__) {
   const tron = Reactotron
     //.setAsyncStorageHandler(AsyncStorage)
     .configure({ host: '192.168.176.144' })
     .useReactNative()
-    .use(asyncStorage())
+    //.use(asyncStorage())
     .use(reactotronRedux())
     .use(reactotronSaga())
     .connect();
