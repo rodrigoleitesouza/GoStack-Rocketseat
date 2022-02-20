5a5df48a558eed0541c329ff40e98108c0699a3a
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-02-20 12:38:19 -0300

    Working n' i'm happy!

--- module10/src/pages/SignIn/index.js
+++ module10/src/pages/SignIn/index.js
@@ -1,17 +1,23 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch } from 'react-redux';
 
 import logo from '../../assets/logo.png';
 
 import Background from '../../components/Background';
+import { signInRequest } from "../../store/modules/auth/actions";
 
 import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles.js';
 
 export default function SignIn({ navigation }) {
+  const dispatch = useDispatch();
   const passwordRef = useRef();
 
-  function handleSubmit() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  function handleSubmit() {
+    dispatch(signInRequest(email, password));
   }
 
   return (
@@ -28,6 +34,8 @@ export default function SignIn({ navigation }) {
             placeholder="Digite seu e-mail"
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
 
           <FormInput
@@ -37,6 +45,8 @@ export default function SignIn({ navigation }) {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
           <SubmitButton onPress={handleSubmit}>
--- module10/src/store/modules/auth/sagas.js
+++ module10/src/store/modules/auth/sagas.js
@@ -42,7 +42,6 @@ export function* signUp({ payload }) {
       name,
       email,
       password,
-      provider: true,
     });
 
     //history.push("/");
