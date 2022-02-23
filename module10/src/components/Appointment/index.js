a48676de8690779d0e818bdaa8a22c16c4fa27b8
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-02-23 19:36:46 -0300

    There's no way!

--- module10/src/components/Appointment/index.js
+++ module10/src/components/Appointment/index.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from "date-fns/locale/pt";
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Left, Avatar, Name, Info, Time } from './styles';
 
-export default function Appointment() {
+export default function Appointment({ data, onCancel }) {
+  const dateParsed = useMemo(
+    () => {
+      return formatRelative(parseISO(data.date), new Date(), {
+        locale: pt,
+        addSuffix: true,
+      })
+    },
+    [data.date],
+  );
+
   return (
-    <Container>
+    <Container past={data.past}>
       <Left>
-        <Avatar source={{ uri: 'https://i.ibb.co/fnZY4Nc/244412150-4339573332789091-124622265567051095-n-Copia.jpg' }}/>
+        <Avatar source={{
+          uri:
+            data.provider.avatar ? data.provider.avatar.url : 'https://i.ibb.co/fnZY4Nc/244412150-4339573332789091-124622265567051095-n-Copia.jpg'
+        }} />
 
         <Info>
-          <Name>Rodrigo Souza</Name>
-          <Time>em 3 horas</Time>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParsed}</Time>
         </Info>
       </Left>
 
-      <TouchableOpacity onPress={() => {}}>
-        <Icon name="event-busy" size={20} color="#F64C75" />
-      </TouchableOpacity>
+      {data.cancelable && !data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#F64C75" />
+        </TouchableOpacity>
+      )}
 
     </Container>
   );
--- module10/src/pages/Dashboard/index.js
+++ module10/src/pages/Dashboard/index.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
 import Background from '../../components/Background';
 import Appointment from '../../components/Appointment';
 
 import { Container, List } from './styles';
 
-const data = [1, 2, 3, 4, 5];
-
 export default function Dashboard() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+
+      setAppointments(response.data);
+
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    setAppointments(
+      appointments.map(appointment =>
+        appointment.id === id
+          ? {
+              ...appointment,
+              canceled_at: response.data.canceled_at,
+            }
+          : appointment
+      )
+    );
+  }
+
   return (
-  <Background>
-    <Container>
-      <List
-        data={data}
-        keyExtractor={item => String(item)}
-        renderItem={({ item }) => <Appointment data={item} />}
-      />
-    </Container>
-  </Background>);
+    <Background>
+      <Container>
+        <List
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => <Appointment onCancel={() => handleCancel(item.id)} data={item} />}
+        />
+      </Container>
+    </Background>);
 }
