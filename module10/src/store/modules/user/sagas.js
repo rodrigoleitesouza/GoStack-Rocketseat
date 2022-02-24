6ed2afc3febb92b6fa7020a07a106465438d85f1
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-02-24 19:12:26 -0300

    Doing goodie!

--- module10/src/store/modules/user/sagas.js
+++ module10/src/store/modules/user/sagas.js
@@ -1,16 +1,15 @@
 import { Alert } from "react-native";
 import { takeLatest, call, put, all } from "redux-saga/effects";
-//import { toast } from "react-toastify";
 import api from "~/services/api";
 
 import { updateProfileSuccess, updateProfileFailure } from "./actions";
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, avatar_id, ...rest } = payload.data;
+    const { name, email, ...rest } = payload.data;
 
     const profile = Object.assign(
-      { name, email, avatar_id },
+      { name, email },
       rest.oldPassword ? rest : {}
     );
 
