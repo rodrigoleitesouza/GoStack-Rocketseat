f5761aa6d56f6454271ad34c56f9f081bb74240f
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-03-02 11:42:15 -0300

    Returning!

--- module10/src/pages/New/SelectProvider/index.js
+++ module10/src/pages/New/SelectProvider/index.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import api from '../../../services/api';
 
 import Background from '../../../components/Background';
 
-// import { Container } from './styles';
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
 
-export default function SelectProvider() {
-  return <Background />;
+export default function SelectProvider({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider onPress={() => navigation.navigate('SelectDateTime', { provider })}>
+              <Avatar source={{
+                uri:
+                provider.avatar ? provider.avatar.url : 'https://i.ibb.co/fnZY4Nc/244412150-4339573332789091-124622265567051095-n-Copia.jpg'
+              }} />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
