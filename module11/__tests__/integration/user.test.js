2107931b8b0d1be987cddd66ba52e4ba1aec171e
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-03-17 16:52:23 -0300

    Truncating!

--- module11/__tests__/integration/user.test.js
+++ module11/__tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +18,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with a duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: "Rodrigo Souza",
+        email: "rodrigosouza@gmail.com",
+        password_hash: "rodrigosouza"
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: "Rodrigo Souza",
+        email: "rodrigosouza@gmail.com",
+        password_hash: "rodrigosouza"
+      });
+
+      expect(response.status).toBe(400);
   });
 });
