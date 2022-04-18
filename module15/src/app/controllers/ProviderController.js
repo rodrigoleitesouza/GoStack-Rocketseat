b70031e35fa8903bef961660c447b09a49a09659
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-04-18 19:36:58 -0300

    Caching!

--- module15/src/app/controllers/ProviderController.js
+++ module15/src/app/controllers/ProviderController.js
@@ -1,22 +1,32 @@
 import User from "../models/User";
 import File from "../models/File";
 
-class ProviderController {
-    async index(req, res) {
-        const providers = await User.findAll({
-            where: { provider: true },
-            attributes: ["id", "name", "email", "avatar_id"],
-            include: [
-                {
-                    model: File,
-                    as: "avatar",
-                    attributes: ["name", "path", "url"],
-                },
-            ],
-        });
+import Cache from "../../lib/Cache";
 
-        return res.json(providers);
+class ProviderController {
+  async index(req, res) {
+    const cached = await Cache.get('providers');
+
+    if (cached) {
+      return res.json(cached);
     }
+
+    const providers = await User.findAll({
+      where: { provider: true },
+      attributes: ["id", "name", "email", "avatar_id"],
+      include: [
+        {
+          model: File,
+          as: "avatar",
+          attributes: ["name", "path", "url"],
+        },
+      ],
+    });
+
+    await Cache.set('providers', providers);
+
+    return res.json(providers);
+  }
 }
 
 export default new ProviderController();
