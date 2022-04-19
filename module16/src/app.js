e82e462837081b43de70d19d8fb47715606547b8
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-04-19 18:20:52 -0300

    Helmeting!

--- module16/src/app.js
+++ module16/src/app.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import path from "path";
 import cors from "cors";
+import helmet from 'helmet';
 import Youch from "youch";
 import * as Sentry from "@sentry/node";
 import "express-async-errors";
@@ -17,7 +18,6 @@ class App {
 
     Sentry.init(sentryConfig);
 
-
     this.middlewares();
     this.routes();
     this.exceptionHandler();
@@ -26,6 +26,7 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
+    this.server.use(helmet());
     this.server.use(express.json());
     this.server.use(
       "/files",
