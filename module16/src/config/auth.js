14cba412b8fa65d72f9119e0b96bcedf5eca377f
Rodrigo Souza <rodrigoleitesouza@gmail.com>
2022-04-19 18:14:20 -0300

    Module16 created!

---
+++ module16/src/config/auth.js
@@ -0,0 +1,7 @@
+export default {
+    secret: process.env.APP_SECRET,
+    expiresIn: "7d",
+};
+
+
+// secret: "f29618255c309de4469993cce24286ea",
---
+++ module16/src/config/sentry.js
@@ -0,0 +1,6 @@
+export default {
+  dsn: process.env.SENTRY_DSN,
+};
+
+
+// dsn: "https://a1e45d4b2b9646b389c2c7f7578c944b@o1118137.ingest.sentry.io/6151908",
